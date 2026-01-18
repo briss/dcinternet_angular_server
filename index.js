2dacfe025b1f32cd608c42ba1f16f7958a6dc3a0
brissgs <briseidags@gmail.com>
2026-01-17 22:20:12 -0600

    Clase 11: Agregar roles y endpoint para refrescar token

--- index.js
+++ index.js
@@ -4,14 +4,19 @@ const cors = require('cors');
 
 const app = express();
 
+const tokenSecret = "My$ecr3t";
+const refreshTokenSecret = "MyR3fr3$hS3cret";
+
 const usuarios = [
     {
         "username": "Briseida",
-        "password": "Br1$$"
+        "password": "Br1$$",
+        "rol": "USUARIO"
     },
     {
         "username": "Christian",
-        "password": "Ch1$"
+        "password": "Ch1$",
+        "rol": "ADMIN"
     }
 ];
 
@@ -55,17 +60,85 @@ app.post('/login', (req, res) => {
 
     console.log('Usuario: ' + usuario + ", Password: " + password);
 
-    let existeUsuario = usuarios.find((u) => u.username === usuario &&
+    let usuarioEncontrado = usuarios.find((u) => u.username === usuario &&
                 u.password === password);
-    if (existeUsuario) {
-        let token = jwt.sign({"user": usuario}, "llaveprivada");
+    if (usuarioEncontrado) {
+        let token = jwt.sign(
+            {"user": usuario}, 
+            "llaveprivada",
+            {
+                expiresIn: '1m'
+            }
+        );
+
+        let refreshToken = jwt.sign(
+            {"user": usuario}, 
+            "nuevoToken",
+            {
+                expiresIn: '1d'
+            }
+        );
+
         res.json({
             "token": token,
+            "refreshToken": refreshToken,
+            "rol": usuarioEncontrado.rol,
             "success": true
         });
     } else {
         res.json({
             success: false
+        });
+    }
+});
+
+app.post('/refresh', (req, res) => {
+    console.log('Refrescando...');
+    let auth = req.headers['authorization'];
+    if (!auth) {
+        res.status(401)
+            .json({
+                "error": "Usuario no autorizado"
+            });
+    }
+    let refreshToken = auth.split(" ")[1];
+
+    try {
+        if (jwt.verify(refreshToken, "nuevoToken")) {
+            let usuario = req.body.usuario;
+
+            let token = jwt.sign(
+                {"user": usuario}, 
+                "llaveprivada",
+                {
+                    expiresIn: '1m'
+                }
+            );
+
+            let refreshToken = jwt.sign(
+                {"user": usuario}, 
+                "nuevoToken",
+                {
+                    expiresIn: '1d'
+                }
+            );
+
+            res.json({
+                "token": token,
+                "refreshToken": refreshToken,
+                "rol": 'REFRESH',
+                "success": true
+            });
+        } else {
+            res.json({
+                "error": "Token incorrecto"
+            });
+        }
+    } catch {
+        res.status(403);
+        res.json({
+            "success": false,
+            "error": "Token incorrecto"
         });
     }
 });
